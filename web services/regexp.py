d48dd1f736560d6de839fde72167dbd73bac6698
CUzkov <UskovDanek@gmail.com>
2020-08-17 16:53:22 +0300

    regexp

--- web services/regexp.py
+++ web services/regexp.py
@@ -1,9 +1,20 @@
+import re
+
+
 def calculate(data, findall):
-    matches = findall(r"")  # Если придумать хорошую регулярку, будет просто
-    for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1,
-         [sign]=, [var2], [[+-]number]
-        # Если бы могло быть только =, вообще одной строкой все считалось
-        # бы, вот так:
-        data[v1] = data.get(v2, 0) + int(n or 0)
+    matches = findall(
+        r"([a-c]{1})([+-]*=)([-+]*\d+|[a-c]{1})([-+]*\d+)*"
+    )
+    for v1, s, v2, n in matches:
+        if bool(re.findall(r"^([+\-]*\d+)$", v2)):
+            buffer = int(v2 or 0)
+        else:
+            buffer = data.get(v2, 0) + int(n or 0)
+        if(s == '='):
+            data[v1] = buffer
+        elif s == '-=':
+            data[v1] -= buffer
+        elif s == '+=':
+            data[v1] += buffer
 
     return data
