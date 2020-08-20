041a998ce1e829934ccfb4492870095ad0d5083c
CUzkov <UskovDanek@gmail.com>
2020-08-20 20:30:41 +0300

    Delete trash

--- item/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(Product)
-admin.site.register(Tag)
-admin.site.register(Feedback)
--- item/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from item import views
-
-urlpatterns = [
-    url('', views.index, name='landing'),
-]
